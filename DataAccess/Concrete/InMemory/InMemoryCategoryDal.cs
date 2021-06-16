f794ad10e331721085f1e8905f74c0204862679a
ahmetozcan999 <f111216079@ktun.edu.tr>
2021-06-16 15:43:10 +0300

    Global Entity Repository Pattern Added

---
+++ DataAccess/Concrete/InMemory/InMemoryCategoryDal.cs
@@ -0,0 +1,47 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCategoryDal:ICategoryDal
+    {
+        List<Category> _category;
+
+        public InMemoryCategoryDal()
+        {
+            _category = new List<Category>
+            {
+                new Category{CategoryId=1,CategoryName="kategori 1"},
+                new Category{CategoryId=2,CategoryName="Kategori 2"},
+                new Category{CategoryId=3,CategoryName="Kategori 3"},
+                new Category{CategoryId=4,CategoryName="Kategori 4"},
+                new Category{CategoryId=5,CategoryName="Kategori 5"},
+                new Category{CategoryId=6,CategoryName="Kategori 6"}
+
+            };
+        }
+
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
