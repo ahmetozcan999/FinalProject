6edb37268788813dd4c5d155d1628ec1694168d9
ahmetozcan999 <f111216079@ktun.edu.tr>
2021-06-17 03:57:02 +0300

    implement protection added for repository pattern

--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -5,12 +5,8 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface ICategoryDal
+    public interface ICategoryDal:IEntityRepository<Category>
     {
-        List<Product> GetAll();
-        void Add(Product product);
-        void Update(Product product);
-        void Delete(Product product);
-        List<Product> GetAllByCategory(int categoryId);
+       
     }
 }
